5f25f3c86f648959d90efea315ff7851670e857c
Mozart Soares <mozartarmstrong@gmail.com>
2023-11-26 20:32:36 -0300

    -Refatoração: Estilização migrada para Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { ThemeProvider } from 'styled-components'
+
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
-
-import './global.css'
+import EstiloGlobal, { Container } from './styles'
+import DefaultTheme from './themes/defaultTheme'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={DefaultTheme}>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
